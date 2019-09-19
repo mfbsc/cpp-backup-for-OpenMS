ffd8c638c6c89b7d1adbbfec93f5b4b7c43d2db7
mf <44728847+mfbsc@users.noreply.github.com>
2019-09-19 10:14:55 +0200

    Stringvectors for SQL database feature table ready
    Next: Build table and insert values

--- FeatureSQLFile.cpp
+++ FeatureSQLFile.cpp
@@ -35,294 +35,444 @@
 #include <OpenMS/KERNEL/FeatureMap.h>
 #include <OpenMS/FORMAT/SqliteConnector.h>
 #include <OpenMS/FORMAT/FeatureSQLFile.h>
-#include <sqlite3.h>
-#include <sstream>
 
-#include <iostream>
-
-
-#include <OpenMS/FORMAT/IdXMLFile.h>
 #include <OpenMS/METADATA/DataProcessing.h>
+#include <OpenMS/METADATA/MetaInfoInterface.h>
+#include <OpenMS/METADATA/MetaInfoInterfaceUtils.h>
+
+
+#include <OpenMS/CONCEPT/UniqueIdInterface.h>
 #include <OpenMS/DATASTRUCTURES/DateTime.h>
 #include <OpenMS/DATASTRUCTURES/Param.h>
-#include <OpenMS/CHEMISTRY/ProteaseDB.h>
+
+#include <sqlite3.h>
+#include <sstream>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include <OpenMS/FORMAT/FileHandler.h>
 
+// #include <Boost>
 
 
 namespace OpenMS
 {
-  /*
-  FeatureSQLFile::FeatureSQLFile()
-  {
-    
-  }
+  // helper functions:  
+  //std::string enumToPrefix(const DataValue::DataType& dt)
+  // translocate strict to FeatureSQLFile.h
 
-  FeatureSQLFile::~FeatureSQLFile()
+  // convert int of enum datatype to  
+  PrefixSQLTypePair enumToPrefix(const int dt)
   {
-
+    // create label prefix according to type
+    std::string prefix = "";
+    std::string type = "";
+    PrefixSQLTypePair pSTP;
+    switch(dt){
+      case 0:
+        pSTP.prefix = "_S_";
+        pSTP.sqltype = "TEXT";
+        break;
+      case 1:
+        pSTP.prefix = "_I_";
+        pSTP.sqltype = "INTEGER";
+        break;
+      case 2:
+        pSTP.prefix = "_D_";
+        pSTP.sqltype = "FLOAT";
+        break;
+      case 3:
+        pSTP.prefix = "_SL_";
+        pSTP.sqltype = "TEXT";
+        break;
+      case 4:
+        pSTP.prefix = "_IL_";
+        pSTP.sqltype = "TEXT";
+        break;
+      case 5:
+        pSTP.prefix = "_DL_";
+        pSTP.sqltype = "TEXT";
+        break;
+      case 6:
+        pSTP.prefix = "";
+        pSTP.sqltype = "TEXT";
+        break;
+    }
+    return pSTP;
   }
+         
+
+/*
+  // convert int of enum datatype to  
+  std::string enumToPrefix(const int dt)
+  {
+    // create label prefix according to type
+    std::string prefix = "";
+    std::string type = "";
+    PrefixSQLTypePair pSTP;
+    switch(dt){
+      case 0:
+        prefix = "_S_";
+        type = "TEXT";
+        break;
+      case 1:
+        prefix = "_I_";
+        type = "INTEGER";
+        break;
+      case 2:
+        prefix = "_D_";
+        type = "FLOAT";
+        break;
+      case 3:
+        prefix = "_SL_";
+        type = "TEXT";
+        break;
+      case 4:
+        prefix = "_IL_";
+        type = "TEXT";
+        break;
+      case 5:
+        prefix = "_DL_";
+        type = "TEXT";
+        break;
+      case 6:
+        prefix = "";
+        break;
+    }
+    return prefix;
+  }
+*/
+
+
+  /* 
+  // Open database
+  SqliteConnector conn(filename);
+
+  // Create SQL structure
+  const char* create_sql =
+    "CREATE TABLE VERSION(" \
+    "ID INT NOT NULL);" \
+
+    // data_processing table
+    "CREATE TABLE DATA_PROCESSING(" \
+    "ID INT PRIMARY KEY NOT NULL," \
+    "SOFTWARE TEXT NOT NULL," \  //get Enum
+    "DATA TEXT NOT NULL," \
+    "TIME TEXT NOT NULL," \
+    "ACTIONS TEXT NOT NULL);" \
+
+  // Execute SQL create statement
+  conn.executeStatement(create_sql);
+
   */
 
-  vector<String>& FeatureSQLFile::getDuplicateLabels(const std::vector<String>& feature_elements, std::vector<String>& userparam_elements)
+  // store FeatureMap as SQL database
+  // fitted snippet of FeatureXMLFile::load
+  void FeatureSQLFile::write(const std::string& out_fm, const FeatureMap& feature_map) const
   {
-    std::vector<String> sorted_feature_v = std::sort(feature_elements.begin(), feature_elements.end())
-    std::vector<String> sorted_userparam_v = std::sort(userparam_elements.begin(), userparam_elements.end())
-    std::vector<String> v_intersection;
+    //sqlite3 *db;
+    //sqlite3_stmt * cntstmt;
+    //sqlite3_stmt * stmt;
+    //std::string select_sql;
+    std::string path="/home/mkf/Development/OpenMS/src/tests/class_tests/openms/data/";
+    std::string filename = path.append(out_fm);
+    //SqliteConnector conn(filename);
 
-    std::set_intersection(sorted_feature_v.begin(), sorted_feature_v.end(), sorted_userparam_v.begin(), sorted_userparam_v.end(), std::back_inserter(v_intersection));
+    std::vector<std::string> feature_elements = {"ID", "RT", "MZ", "Intensity", "Charge", "Quality"};
+    std::vector<std::string> feature_elements_types = {"INTEGER", "REAL", "REAL", "REAL", "INTEGER", "REAL"};
+    
+    // read feature_map values and insert into map
+    std::set<String> common_keys = MetaInfoInterfaceUtils::findCommonMetaKeys<FeatureMap, std::set<String> >(feature_map.begin(), feature_map.end(), 0.0);
 
-    for (const string& elem : v_intersection)
+    std::map<String, DataValue::DataType> map_key2type;
+
+    for (auto feature : feature_map)
     {
-      if (v_intersection == sorted_userparam_v[elem])
+      for (const String& key : common_keys)
       {
-        sorted_userparam_v[elem] == sorted_userparam_v[elem].insert(0, 1, '_');
+        if (feature.metaValueExists(key))
+        {
+          const DataValue::DataType& dt = feature.getMetaValue(key).valueType();
+          map_key2type[key] = dt;
+        }
       }
-    } 
-    
-    return sorted_userparams_v;
-  }
-
-  vector<String>& FeatureSQLFile::prefixLabel(vector<String> types of header)
-  {
-    // return vector label, vector column_type
-  }
-
-  vector<String> FeatureSQLFile::formatSQLEntries()
-  {
-    std::vector<String> column_label_v;
-    std::vector<String> column_type_v;
-    
-    for ()
-
-  }
-
-
-
-
-
-
-  // read SQL database and store as FeatureMap
-  FeatureMap FeatureSQLFile::read(const std::string& filename) const
-  {
-    if (!File::exists(filename))
-    {
-      throw Exception::FileNotFound(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, filename);
     }
 
-    if (!File::readable(filename))
+    for (const String& key : common_keys)
     {
-      throw Exception::FileNotReadable(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, filename);
+      //std::cout << key << "\t" << map_key2type[key] << std::endl;
+      std::cout << key << "\t" << enumToPrefix(map_key2type[key]).prefix << std::endl;
+      // build feature_elements vector with prefixes and keys
+      feature_elements.push_back(enumToPrefix(map_key2type[key]).prefix += key);
+      // build feature_elements type vector for SQL stmt
+      feature_elements_types.push_back(enumToPrefix(map_key2type[key]).sqltype);
     }
-
-    if (infile_.is_open()) infile_.close(); // precaution
-
-    infile_.open(filename.c_str(), std::ios::binary | std::ios::in);
-
-
-  }
-
-  // store FeatureXML as SQL database
-  void FeatureSQLFile::write(const std::string& out_fm, const FeatureMap& fm) const
-  {
     
+    
+
+    // Iteration over FeatureMap
+    // turn into line feed function for SQL database
+    for (auto it = feature_map.begin(); it != feature_map.end(); ++it)
+    {
+      std::cout << it->getRT() << "\t" << it->getMZ() <<  std::endl;
+    }
+  
+  std::ofstream outfile(filename);
+  outfile << "Test";
+  outfile.close();
   }
+}
+
 
   /*
-  void FeatureSQLFile::read(const std::string& in_xml)
-    {
-      sqlite3_stmt * stmt;
-      std::string select_sql;
 
-      // Open database
-      SqliteConnector conn(in_xml);
 
-      if ()
-      {
-        select_sql = "SELECT * FROM ".append(table_name)
-      }
-      else if
-      {
-        std::cout << 
-      }
+  }
 
+  // populate SQL database with map entries
+  // delete file if present
+  remove(filename);
+
+  // Open database
+  SqliteConnector conn(filename);
+
+  // Create SQL structure
+  const char* create_sql =
+    "CREATE TABLE VERSION(" \
+    "ID INT NOT NULL);" \
+
+    // data_processing table
+    "CREATE TABLE DATA_PROCESSING(" \
+    "ID INT PRIMARY KEY NOT NULL," \
+    "SOFTWARE TEXT NOT NULL," \  //get Enum
+    "DATA TEXT NOT NULL," \
+    "TIME TEXT NOT NULL," \
+    "ACTIONS TEXT NOT NULL);" \
+
+  // Execute SQL create statement
+  conn.executeStatement(create_sql);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+  // read SQL database and store as FeatureMap
+  // based on TransitionPQPFile::readPQPInput_
+
+  FeatureMap FeatureSQLFile::read(const std::string& filename) const
+  {
+    sqlite3 *db;
+    sqlite3_stmt * cntstmt;
+    sqlite3_stmt * stmt;
+    std::string select_sql;
+    SqliteConnector conn(filename);
+    
+    // connect database and read tables
+    db = conn.getDB();
+
+    //create empty FeatureMap object
+    FeatureMap feature_map;
+
+    // get table entries if existent
+    bool data_processing_exists = SqliteConnector::tableExists(db, "DATA_PROCESSING");
+    if (data_processing_exists)
+    {    
       // Execute SQL select statement
       conn.executePreparedStatement(&stmt, select_sql);
       sqlite3_step( stmt );
 
-      int cols = sqlite3_column_count(stmt);
-
-      // Generate features
-      int k = 0;
-      std::vector<std::string> group_id_index;
-
       while (sqlite3_column_type( stmt, 0 ) != SQLITE_NULL)
       {
-        int label = 0;
-        std::map<std::string, double> features;
+        setProgress(progress++);
+        Feature current_feature;
 
-        for (int i = 0; i < cols; i++)
-        {
-          if (OpenMS::String(sqlite3_column_name( stmt, i )) == "FEATURE_ID")
-          {
-            psm_id = OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i )));
-          }
-          if (OpenMS::String(sqlite3_column_name( stmt, i )) == "GROUP_ID")
-          {
-            if (std::find(group_id_index.begin(), group_id_index.end(),
-                  OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i )))) != group_id_index.end())
-            {
-              scan_id = std::find(group_id_index.begin(), group_id_index.end(),
-                          OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i )))) - group_id_index.begin();
-            }
-            else
-            {
-              group_id_index.push_back(OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i ))));
-              scan_id = std::find(group_id_index.begin(), group_id_index.end(),
-                          OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i )))) - group_id_index.begin();
-            }
-          }
-          if (OpenMS::String(sqlite3_column_name( stmt, i )) == "DECOY")
-          {
-            if (sqlite3_column_int( stmt, i ) == 1)
-            {
-              label = -1;
-            }
-            else
-            {
-              label = 1;
-            }
-          }
-          if (OpenMS::String(sqlite3_column_name( stmt, i )) == "MODIFIED_SEQUENCE")
-          {
-            peptide = OpenMS::String(reinterpret_cast<const char*>(sqlite3_column_text( stmt, i )));
-          }
-          if (OpenMS::String(sqlite3_column_name( stmt, i )).substr(0,4) == "VAR_")
-          {
-            features[OpenMS::String(sqlite3_column_name( stmt, i ))] = sqlite3_column_double( stmt, i );
-          }
-        }
+        Sql::extractValue<double>(&current_feature.setUniqueId(), stmt, 0);
+        Sql::extractValue<double>(&mytransition.library_intensity, stmt, 5);
+        Sql::extractValue<std::string>(&mytransition.group_id, stmt, 6);
 
-        // Write output
-        if (k == 0)
-        {
-          pin_output << "PSMId\tLabel\tScanNr";
-          for (auto const &feat : features)
-          {
-            pin_output << "\t" << feat.first;
-          }
-          pin_output << "\tPeptide\tProteins\n";
-        }
-        pin_output << psm_id << "\t" << label << "\t" << scan_id;
-        for (auto const &feat : features)
-        {
-          pin_output << "\t" << feat.second;
-        }
-        pin_output << "\t." << peptide << ".\tProt1" << "\n";
-
-        sqlite3_step( stmt );
-        k++;
-      }
-
-      sqlite3_finalize(stmt);
-
-      if (k==0)
-      {
-        if (osw_level == "transition")
-        {
-        }
-        else
-        {
-          throw Exception::FileEmpty(__FILE__, __LINE__, __FUNCTION__, in_osw);
-        }
       }
 
     }
 
-
-
-    void FeatureSQLFile::write(const std::string& in_osw,const std::map< std::string, std::vector<double> >& features)
+    bool features_exists = SqliteConnector::tableExists(db, "FEATURES");
+    if (features_exists)
     {
-      std::string table;
-      std::string create_sql;
+    }
 
-      if (osw_level == "")
-      {
-        table_dataproc = "dataproc";
-        create_sql =  "DROP TABLE IF EXISTS " << table_dataproc << "; " \
-                      "CREATE TABLE " << table_dataproc << "(" \
-                      "PEP DOUBLE NOT NULL);";
-        table_subordinates = "subordinates";
-        create_sql =  "DROP TABLE IF EXISTS " << table_subordinates << "; " \
-                      "CREATE TABLE " << table_subordinates << "(" \
-        table_features = "features";
-        create_sql =  "DROP TABLE IF EXISTS " << table_features << "; " \
-                      "CREATE TABLE " << table_features << "(" \
+    bool subordinates_exists = SqliteConnector::tableExists(db, "SUBORDINATES");
+    if (subordinates_exists)
+    {
+      // if column FEATURE_REF exists  
+      bool feature_ref = SqliteConnector::columnExists(db, "SUBORDINATES", "FEATURE_REF");
 
-
-      std::vector<std::string> insert_sqls;
-      for (auto const &feat : features)
-      {
-        std::stringstream insert_sql;
-        if (osw_level == "transition") {
-          std::vector<OpenMS::String> ids;
-          OpenMS::String(feat.first).split("_", ids);
-          insert_sql << "INSERT INTO " << table;
-          insert_sql << " (FEATURE_ID, TRANSITION_ID, SCORE, QVALUE, PEP) VALUES (";
-          insert_sql <<  ids[0] << ",";
-          insert_sql <<  ids[1] << ",";
-          insert_sql <<  feat.second[0] << ",";
-          insert_sql <<  feat.second[1] << ",";
-          insert_sql <<  feat.second[2] << "); ";
-        }
-        else
-        {
-          insert_sql << "INSERT INTO " << table;
-          insert_sql << " (FEATURE_ID, SCORE, QVALUE, PEP) VALUES (";
-          insert_sql <<  feat.first << ",";
-          insert_sql <<  feat.second[0] << ",";
-          insert_sql <<  feat.second[1] << ",";
-          insert_sql <<  feat.second[2] << "); ";
-        }
-
-        insert_sqls.push_back(insert_sql.str());
-      }
-
-      // Write to Sqlite database
-      SqliteConnector conn(in_osw);
-      conn.executeStatement(create_sql);
-      conn.executeStatement("BEGIN TRANSACTION");
-      for (size_t i = 0; i < insert_sqls.size(); i++)
-      {
-        conn.executeStatement(insert_sqls[i]);
-      }
-      conn.executeStatement("END TRANSACTION");
     }
 
 
+    // import features and subordinates
 
-
+    // save data as featureMap
+  }
   */
-} // namespace OpenMS
+//} // namespace OpenMS
+
 
 
 
 /*
+// https://www.geeksforgeeks.org/sql-using-c-c-and-sqlite/
+//database access
 
-// try to establish database connection
-SqliteConnector get_DB_connection(const std::string& db_filename)
-{
+{ 
+    sqlite3* DB; 
+    int exit = 0; 
+    exit = sqlite3_open("example.db", &DB); 
   
-}
+    if (exit) { 
+        std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl; 
+        return (-1); 
+    } 
+    else
+        std::cout << "Opened Database Successfully!" << std::endl; 
+    sqlite3_close(DB); 
+    return (0); 
+} 
 
-datatype readTable(SqliteConnector connection, const std::string table_name)
-{
+// create 3 databases depending upon number of FeatureMap depth
+// 1. dataprocessing
+// 2. features
+// 3. subordinates
+{ 
+    sqlite3* DB; 
+    std::string sql = "CREATE TABLE dataprocessing("
+                      "ID INT PRIMARY KEY     NOT NULL, "
+                      "NAME           TEXT    NOT NULL, "
+                      "SURNAME        TEXT    NOT NULL, "
+                      "AGE            INT     NOT NULL, "
+                      "ADDRESS        CHAR(50), "
+                      "SALARY         REAL );"; 
+    
+    int exit = 0; 
+    exit = sqlite3_open("example.db", &DB); 
+    char* messaggeError; 
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); 
+  
+    if (exit != SQLITE_OK) { 
+        std::cerr << "Error Create Table" << std::endl; 
+        sqlite3_free(messaggeError); 
+    } 
+    else
+        std::cout << "Table created Successfully" << std::endl; 
+    sqlite3_close(DB); 
+    return (0); 
+} 
 
-}
 
-datatype getHeader(table)
-{
+// insert and delete
 
-}
+static int callback(void* data, int argc, char** argv, char** azColName) 
+{ 
+    int i; 
+    fprintf(stderr, "%s: ", (const char*)data); 
+  
+    for (i = 0; i < argc; i++) { 
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL"); 
+    } 
+  
+    printf("\n"); 
+    return 0; 
+} 
+  
+int main(int argc, char** argv) 
+{ 
+    sqlite3* DB; 
+    char* messaggeError; 
+    int exit = sqlite3_open("example.db", &DB); 
+    string query = "SELECT * FROM PERSON;"; 
+  
+    cout << "STATE OF TABLE BEFORE INSERT" << endl; 
+  
+    sqlite3_exec(DB, query.c_str(), callback, NULL, NULL); 
+  
+    string sql("INSERT INTO PERSON VALUES(1, 'STEVE', 'GATES', 30, 'PALO ALTO', 1000.0);"); 
+  
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); 
+    if (exit != SQLITE_OK) { 
+        std::cerr << "Error Insert" << std::endl; 
+        sqlite3_free(messaggeError); 
+    } 
+    else
+        std::cout << "Records created Successfully!" << std::endl; 
+  
+    cout << "STATE OF TABLE AFTER INSERT" << endl; 
+  
+    sqlite3_exec(DB, query.c_str(), callback, NULL, NULL); 
+  
+    sql = "DELETE FROM PERSON WHERE ID = 2;"; 
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError); 
+    if (exit != SQLITE_OK) { 
+        std::cerr << "Error DELETE" << std::endl; 
+        sqlite3_free(messaggeError); 
+    } 
+    else
+        std::cout << "Record deleted Successfully!" << std::endl; 
+  
+    cout << "STATE OF TABLE AFTER DELETE OF ELEMENT" << endl; 
+    sqlite3_exec(DB, query.c_str(), callback, NULL, NULL); 
+  
+    sqlite3_close(DB); 
+    return (0); 
+} 
 
+
+
+// select operation
+int main(int argc, char** argv) 
+{ 
+    sqlite3* DB; 
+    int exit = 0; 
+    exit = sqlite3_open("example.db", &DB); 
+    string data("CALLBACK FUNCTION"); 
+  
+    string sql("SELECT * FROM PERSON;"); 
+    if (exit) { 
+        std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl; 
+        return (-1); 
+    } 
+    else
+        std::cout << "Opened Database Successfully!" << std::endl; 
+  
+    int rc = sqlite3_exec(DB, sql.c_str(), callback, (void*)data.c_str(), NULL); 
+  
+    if (rc != SQLITE_OK) 
+        cerr << "Error SELECT" << endl; 
+    else { 
+        cout << "Operation OK!" << endl; 
+    } 
+  
+    sqlite3_close(DB); 
+    return (0); 
+} 
 */
